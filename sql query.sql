3830bc6c79c6f82e97b4d805817e9d31a5742620
Moomal Mahar <moomal.mahar@student.fh-kiel.de>
2019-02-01 00:37:48 +0100

    Add files via upload
    SQL query to populate database

---
+++ sql query.sql
@@ -0,0 +1,34 @@
+create database scheduleparser;
+use scheduleparser;
+INSERT INTO `scheduleparser`.`modules`
+(
+`moduleID`,
+`moduleName`,
+`moduleSemesterOffered`,
+`moduleECTS`,
+`moduleLocation`,
+`moduleDepartment`,
+`createdAt`,
+`updatedAt`)
+VALUES
+("MI102","Network Systems and Security","s",	5	,"FH Kiel","Informatik",now(),now()),
+("MI108","Advanced C++","s",	5	,"FH Kiel","Informatik",now(),now()),
+("MI109","Pattern Recognition","w",	2	,"FH Kiel","Informatik",now(),now()),
+("MI110","IT Security","w",	5	,"FH Kiel","Informatik",now(),now()),
+("MI111","Automatic Speech Processing","w",	4	,"FH Kiel","Informatik",now(),now()),
+("MI113","Automatic Image Analysis","s",	5	,"FH Kiel","Informatik",now(),now()),
+("MI115","Advanced Data Quality Management","s",	5	,"CAU Kiel","Mechatronics",now(),now()),
+("MI116","Neural Networks","s",	3	,"FH Kiel","Mechatronics",now(),now()),
+("MI117","Machine Learning","s",	5	,"FH Kiel","Mechatronics",now(),now()),
+("MI118","Enterprise Architecture","s",	7	,"CAU Kiel","Mechatronics",now(),now()),
+("MI120","Privacy and Data Protection","s",	5	,"FH Kiel","Mechatronics",now(),now()),
+("MI123","Advanced Java","sw",	5	,"FH Kiel","Mechatronics",now(),now()),
+("MI124","Cloud Computing and Security","sw",	4	,"FH Kiel","Mechatronics",now(),now()),
+("MI125","Cognitive Consulting","sw",	5	,"FH Kiel","Economics",now(),now()),
+("MI126","DevOps - Agile in Progress","sw",	5	,"FH Kiel","Economics",now(),now()),
+("MI127","Cloud Services","s",	3	,"CAU Kiel","Economics",now(),now()),
+("MI128","Agile Software Development","w",	5	,"FH Kiel","Economics",now(),now()),
+("MI129","Free Open Source Software Projects","w",	5	,"FH Kiel","Economics",now(),now()),
+("MI130","Web Application Security","sw",	5	,"FH Kiel","Economics",now(),now()),
+("MI133","Modern Web Development","sw",	5	,"FH Kiel","Economics",now(),now()),
+("PM105","Cross-Cultural Leadership Human","s",	5	,"FH Kiel","Economics",now(),now())
